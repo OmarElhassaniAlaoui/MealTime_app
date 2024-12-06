acf8da0987ee487428fa0840f30e9b84a257571c
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2024-12-06 17:41:20 +0000

    code refacroring

--- lib/src/app/themes/app_spacing.dart
+++ lib/src/app/themes/app_spacing.dart
@@ -2,24 +2,32 @@ class AppSpacing {
   AppSpacing._();
 
   // Spacing
-  static double s2 = 2.0;
-  static double s4 = 4.0;
-  static double s8 = 8.0;
-  static double s14 = 14.0;
-  static double s16 = 16.0;
-  static double s18 = 18.0;
-  static double s20 = 20.0;
-  static double s24 = 24.0;
-  static double s36 = 36.0;
-  static double s40 = 40.0;
-  static double s44 = 44.0;
-  static double s48 = 48.0;
-  static double s52 = 52.0;
-  static double s56 = 56.0;
-  static double s60 = 60.0;
-  static double s64 = 64.0;
+  static const double s2 = 2.0;
+  static const double s4 = 4.0;
+  static const double s8 = 8.0;
+  static const double s14 = 14.0;
+  static const double s16 = 16.0;
+  static const double s18 = 18.0;
+  static const double s20 = 20.0;
+  static const double s24 = 24.0;
+  static const double s36 = 36.0;
+  static const double s40 = 40.0;
+  static const double s44 = 44.0;
+  static const double s48 = 48.0;
+  static const double s52 = 52.0;
+  static const double s56 = 56.0;
+  static const double s60 = 60.0;
+  static const double s64 = 64.0;
 
   // Radius
-  static double r7 = 7.0;
-  static double r4 = 4.0;
+  static const double r4 = 4.0;
+  static const double r8 = 8.0;
+  static const double r12 = 12.0;
+  static const double r16 = 16.0;
+  static const double r20 = 20.0;
+  static const double r24 = 24.0;
+  static const double r28 = 28.0;
+  static const double r32 = 32.0;
+  static const double r36 = 36.0;
+
 }
