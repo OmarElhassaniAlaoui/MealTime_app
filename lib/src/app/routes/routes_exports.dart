a3a289a20459128d4b6a8b3d5dbf59033ad3f01d
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2024-12-12 22:31:10 +0000

    app routes- setup

---
+++ lib/src/app/routes/routes_exports.dart
@@ -0,0 +1,3 @@
+export 'package:flutter/material.dart';
+export 'package:go_router/go_router.dart';
+export 'package:meal_time_app/src/presentation/template/pages/template_page.dart';
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,7 +7,6 @@ import Foundation
 
 import connectivity_plus
 import flutter_secure_storage_macos
-import isar_flutter_libs
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -16,7 +15,6 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
