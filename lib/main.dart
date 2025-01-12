6167ad00d19731233b316eda0457d50befe6831a
Omar <o.elhassanialaoui@edu.umi.ac.ma>
2025-01-12 19:47:20 +0000

    Implement BLoC pattern for authentication with cubits and states

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meal_time_app/src/app/helpers/my_cubit_observer.dart';
 import 'package:meal_time_app/src/meal_time_app.dart';
 import 'package:meal_time_app/src/app/core/dependancy-injection/injection_container.dart'
     as di;
 
 void main() async {
+  Bloc.observer = MyBlocObserver();
+
   WidgetsFlutterBinding.ensureInitialized();
 
   di.init();
